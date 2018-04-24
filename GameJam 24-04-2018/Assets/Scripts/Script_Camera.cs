088b8db1aa011deeeb7c9edf1a2fa77f6c6ed467
Frambozenjam <38690460+Frambozenjam@users.noreply.github.com>
2018-04-24 15:10:18 +0200

    First push
    test

---
+++ GameJam 24-04-2018/Assets/Scripts/Script_Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_Camera : MonoBehaviour {
+
+    public GameObject ObjectToFollow;
+    public float FollowDistanceZ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        transform.position = new Vector3(ObjectToFollow.transform.position.x, ObjectToFollow.transform.position.y, ObjectToFollow.transform.position.z + FollowDistanceZ);
+	}
+}
---
+++ GameJam 24-04-2018/Assets/Scripts/Script_PlayerManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_PlayerManager : MonoBehaviour
+{
+
+    public float BoostPower = 1.0f;
+    public float RotationSpeed = 1.0f;
+    public float GravityPower = 1.0f;
+    public float maxGravDist = 4.0f;
+    public float maxGravity = 35.0f;
+    public Rigidbody2D rb;
+
+    GameObject[] planets;
+
+    void Start()
+    {
+        planets = GameObject.FindGameObjectsWithTag("Planet");
+    }
+
+    void FixedUpdate()
+    {
+        foreach (GameObject planet in planets)
+        {
+            float dist = Vector3.Distance(planet.transform.position, transform.position);
+            if (dist <= maxGravDist)
+            {
+                Vector3 v = planet.transform.position - transform.position;
+                rb.AddForce(v.normalized * (GravityPower - dist / maxGravDist) * maxGravity);
+            }
+        }
+
+        if (Input.GetButton("Jump"))
+        {
+            rb.AddForce(transform.right * BoostPower * Time.deltaTime);
+        }
+
+        if (Input.GetButton("Horizontal"))
+        {
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                rb.MoveRotation(rb.rotation + RotationSpeed * Time.deltaTime);
+            }
+            else if (Input.GetAxis("Horizontal") > 0)
+            {
+                rb.MoveRotation(rb.rotation + -RotationSpeed * Time.deltaTime);
+            }
+        }
+    }
+}
